4b1546f7e683648082ff78aa9aff7fd5a42af205
Maruf Hossain <maruf.bshs@gmail.com>
2025-04-22 03:36:42 +0600

    added contact page

--- StudySync/StudySync/urls.py
+++ StudySync/StudySync/urls.py
@@ -21,4 +21,6 @@ from SyncEngine import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
